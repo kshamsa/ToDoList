2809c16bd38e57ec77b881a6de5805e96f284d08
kionshamsa <kionkshamsa@gmail.com>
2022-06-16 11:15:47 -0600

    Added html templates.

--- main/urls.py
+++ main/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from .			 import views
 
 urlpatterns = [
-	path("<int:id>",  views.index, name="index"), 
-	#path("",         views.index, name="index"), 
+	path("<int:id>", views.index,  name="index"), 
+	path("",         views.home,   name="home"), 
+	path("create/",  views.create, name="create")
 ]
